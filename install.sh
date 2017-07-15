4391b59e30728f1139ff0a27f2cf88553bff1a97
Ubuntu <ubuntu@ip-172-31-8-190.us-west-2.compute.internal>
2017-07-15 19:42:38 +0000

    improvements

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-
-sudo apt-get install google-chrome-stable
-sudo apt-get install xvfb
-pip install Google-Search-API
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install -y unzip xvfb google-chrome-stable
+pip install Google-Search-API gunicorn flask
 pip install pyvirtualdisplay selenium
 wget https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
+sudo apt-get install unzip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
 sudo mv -f chromedriver /usr/local/share/chromedriver
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gunicorn -b 127.0.0.1:7777 google_search:app
+gunicorn -b 0.0.0.0:7777 google_search:app
